167271eba205510591f75a6f2a0386736e56d44d
ppd05 <piyusa.panda@sky.uk>
2023-08-03 08:16:46 +0100

    Added temp code smell - code duplication

--- src/main/kotlin/io/github/u/ways/challenge1.kt
+++ src/main/kotlin/io/github/u/ways/challenge1.kt
@@ -17,3 +17,20 @@ fun challenge1(request: Request, response: String) {
         """.trimIndent()
     )
 }
+
+
+fun challenge1_new2(request: Request, response: String) {
+    println(
+        """
+            I've received the following request: 
+            - Name: ${request.name}
+            - Email: ${request.email}
+            - Phone: ${request.phone}
+            - Address: ${request.address}
+            - Internet: ${request.internet}
+            - VoIP: ${request.voip}
+            - Mobile: ${request.mobile}
+            - Landline: ${request.landline}
+        """.trimIndent()
+    )
+}
